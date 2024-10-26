b0ee7905810b526dd3b69f829e01931095611662
Ayush <ayushrjadhav14@gmail.com>
2024-10-26 09:35:43 -0500

    update to path #2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ayushrjadhav.github.io/',
+  base: '/ayushrjadhav.github.io/', // or '/' if using a custom domain
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
-})
+});
