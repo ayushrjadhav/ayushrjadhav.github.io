02d37401d1a7e2cb752beaf3087fb0c55c3df8d8
Ayush <ayushrjadhav14@gmail.com>
2024-10-26 09:23:30 -0500

    trying to fix MME error

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ayushrjadhav.github.io/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
 })
