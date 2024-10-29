85ce7420c26f440b48d2085224470a43225699c3
Ayush <ayushrjadhav14@gmail.com>
2024-10-29 08:36:48 -0500

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ayushrjadhav.github.io/', // or '/' if using a custom domain
+ // or '/' if using a custom domain
   plugins: [react()],
+  base: '/',
 });
