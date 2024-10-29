c552e7cbad7179ad7da92d3ecfa5024ab0f82e08
Ayush <ayushrjadhav14@gmail.com>
2024-10-29 10:22:29 -0500

    changed vite file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
- // or '/' if using a custom domain
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '.jsx': '.js'
+    }
+  }
 });
